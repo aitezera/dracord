cd16aa9ab58e6fc5ff5d528f5ea7d41d017c04b6
Tom <oerushia@gmail.com>
2024-07-05 18:16:25 +0100

    TODOs to remind myself

--- include/websocket.h
+++ include/websocket.h
@@ -6,6 +6,14 @@
 #include <ixwebsocket/IXUserAgent.h>
 #include <json/json.h>
 
+/*
+
+TODO: Add more events and handle them in handleEvents function
+TODO: Add more intents to INTENTS variable
+TODO: Move requests into websocket and completely remove requests.h
+
+*/
+
 const std::string GATEWAY_URL = "wss://gateway.discord.gg/?v=9&encoding=json";
 const int INTENTS = (1 << 0) | (1 << 9) | (1 << 1) | (1 << 12);
 
